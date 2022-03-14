f7073bc28767507f56d6eead710cb18d73086a6f
sabbir880 <drsabbir880@gmail.com>
2022-03-15 01:09:44 +0600

    first commit

---
+++ js/apps.js
@@ -0,0 +1,25 @@
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => countriesDiteals(data));
+};
+
+const countriesDiteals = countries => {
+    console.log(countries)
+    const countriesHtml = countries.map( country => getCountriesHtml(country));
+    const container =document.getElementById('countries');
+    container.innerHTML = countriesHtml;
+};
+
+const getCountriesHtml = country =>{    
+    // console.log(country)
+    return `
+        <div class="content">
+          <h1>${country.name.common}</h1>
+          <h1>${country.subregion}</h1>
+          <img src="${country.flags.png}">
+        </div>
+    `
+}
+
+loadCountries()
